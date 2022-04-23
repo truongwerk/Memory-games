d450b8b2b64f7393f7e44f9683650903215e9a0f
Tong Quang Truong <truongwerk@gmail.com>
2022-04-23 16:58:38 +0700

    fi

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// vite.config.js
-import legacy from "@vitejs/plugin-legacy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	base: "/Memory-games/",
-	plugins: [
-		react(),
-		legacy({
-			targets: ["defaults", "not IE 11"],
-		}),
-	],
+	plugins: [react()],
 });
