231d0b4f48967d55cd6c1264284297d85b4fc5fb
Tong Quang Truong <truongwerk@gmail.com>
2022-04-23 16:34:19 +0700

    legacy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+// vite.config.js
+import legacy from "@vitejs/plugin-legacy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+	plugins: [
+		react(),
+		legacy({
+			targets: ["defaults", "not IE 11"],
+		}),
+	],
+});
