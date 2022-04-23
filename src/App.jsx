45493e7e25ed31943b70f7b7319ec41592569c33
Tong Quang Truong <truongwerk@gmail.com>
2022-04-23 17:17:08 +0700

    meta

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import { useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import "./styles/reset.css";
+import "./styles/App.css";
 
 function App() {
 	const [count, setCount] = useState(0);
-
 	return (
 		<div className="App">
 			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
 				<p>Hello Vite + React!</p>
 				<p>
 					<button type="button" onClick={() => setCount((count) => count + 1)}>
